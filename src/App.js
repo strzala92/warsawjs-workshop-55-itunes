69b9dce9140c551a56a37d8ce2192af40f4f5600
SebastianStrzelecki <Sebastian.Strzelecki@opi.org.pl>
2021-06-21 18:54:42 +0200

    add fetching

--- src/App.js
+++ src/App.js
@@ -1,34 +1,71 @@
-import logo from './logo.svg';
 import './App.css';
 
+import {
+  Stack,
+  Input,
+  Button,
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+} from "@chakra-ui/react"
+import { useState } from 'react';
 
+function ItunesResult({ props }) {
+  return (
+    <Table variant="simple">
+      <TableCaption>Itunes music</TableCaption>
+      <Thead>
+        <Tr>
+          <Th>Artist Name</Th>
+          <Th>Track Name</Th>
+          <Th>Genre Name</Th>
+        </Tr>
+      </Thead>
+      <Tbody>
+          {props.map((val, index) =>
+            <Tr key={index}>
+              <Td>{val.artistName}</Td>
+              <Td>{val.trackCensoredName}</Td>
+              <Td>{val.primaryGenreName}</Td>
+              </Tr>
+          )}
+      </Tbody>
+    </Table>
+  );
+}
 function Itunes() {
+  const [searchTerm, setSetchTerm] = useState('');
+  const [results, setResults] = useState([]);
+  const handleChange = (event) => setSetchTerm(event.target.value);
+  const getTerms = async () => {
+    const result = await fetch(`https://itunes.apple.com/search?term=${encodeURIComponent(searchTerm)}&entity=musicVideo`);
+    const data = await result.json();
+    setResults(data.results);
+  };
+
   return (
     <Stack direction="column">
-      <Stack>
-        
+      <Stack direction="row">
+        <Input value={searchTerm} onChange={handleChange} />
+        <Button colorScheme="blue" onClick={getTerms}>Search</Button>
       </Stack>
+      <ItunesResult props={results} />
     </Stack>
-  )
+  );
 }
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Itunes />
       </header>
     </div>
+
   );
 }
 
